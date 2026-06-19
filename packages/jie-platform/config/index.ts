c99eaeed36b4e795354905573ca5fd4ab3d9713f
Cause Chung <cuzfrog@gmail.com>
2026-06-18 22:36:44 -0400

    Align model-registry filename

--- packages/jie-platform/config/index.ts
+++ packages/jie-platform/config/index.ts
@@ -6,6 +6,6 @@ export {
   findProjectJieRoot,
   projectSettingsPath,
 } from "./paths.ts";
-export { ModelRegistry } from "./registry.ts";
+export { ModelRegistry } from "./model-registry.ts";
 export { type Scope, type SettingsStore, makeSettingsStore } from "./settings-store.ts";
 export { type AuthStore, makeAuthStore } from "./auth-store.ts";
